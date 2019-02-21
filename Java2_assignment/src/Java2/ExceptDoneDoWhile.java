a08f78fa08f91489d76d4fa34a96f2a0a74e6b6a
shivangi <shivangi.jain@tothenew.com>
2019-02-22 02:57:13 +0530

    question 8

--- Java2_assignment/src/Java2/ExceptDoneDoWhile.java
+++ Java2_assignment/src/Java2/ExceptDoneDoWhile.java
@@ -1,4 +1,7 @@
 package Java2;
+//8. WAP to read words from the keyboard until the word done is entered. For each word except done, report whether its first character is equal   to  its last character. For the required loop, use a
+//        a)while statement
+//        b)do-while statement
 //Question 8b do-while
 import java.util.Scanner;
 
