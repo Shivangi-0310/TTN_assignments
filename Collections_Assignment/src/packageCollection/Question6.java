c9f1f12148971e7711a819fb960e28cf4bbbe317
shivangi <shivangi.jain@tothenew.com>
2019-02-26 15:07:38 +0530

    Question 6

--- Collections_Assignment/src/packageCollection/Question6.java
+++ Collections_Assignment/src/packageCollection/Question6.java
@@ -1,45 +1,38 @@
 package packageCollection;
 //Question 6 Print the elements of an array in the decreasing
 // frequency if 2 numbers have same frequency then print the one which came first.
-import java.util.HashMap;
+
+
 import java.util.*;
+
+
 public class Question6 {
-    public HashMap<Integer,Integer> elementCount(int array[]) {
-        HashMap<Integer, Integer> elementOccurence = new HashMap<>();
-        for (int i : array) {
-            if (!elementOccurence.containsKey(i)) {
-                elementOccurence.put(i, 1);
-            } else {
-                elementOccurence.put(i, elementOccurence.get(i) + 1);
-            }
-        }
-        List<Map.Entry<Integer, Integer> > list = new LinkedList<>(elementOccurence.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer> >() {
-            public int compare(Map.Entry<Integer, Integer> o1,
-                               Map.Entry<Integer, Integer> o2)
-            {
-
-                if(o2.getValue()==o1.getValue())
-                    return o1.getKey();
-                else
-                return (o2.getValue()).compareTo(o1.getValue());
-            }
-        });
-        HashMap<Integer, Integer> temp = new LinkedHashMap<>();
-        for (Map.Entry<Integer, Integer> list1 : list) {
-
-            temp.put(list1.getKey(), list1.getValue());
-        }
-        return temp;
-    }
     public static void main(String[] args) {
-        int array[]={1,3,0,4,3,6,7,5,4,6,6,9,4,3,7};
-        Question6 q = new Question6();
-        HashMap<Integer,Integer> temp =q.elementCount(array);
-        for (Map.Entry<Integer, Integer> i : temp.entrySet()) {
-            System.out.println("Element = " + i.getKey() + ", occurence = " + i.getValue());
+        String string = "this is to sort a string according to character occurrence";
+        Map<Character, Integer> frequency = new LinkedHashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ') {
+                if (frequency.containsKey(string.charAt(i))) {
+                    int k = frequency.get(string.charAt(i));
+                    k++;
+                    frequency.put(string.charAt(i), k);
+                } else {
+                    frequency.put(string.charAt(i), 1);
+                }
+            }
         }
+        List<Map.Entry<Character, Integer>> list = new LinkedList<>(frequency.entrySet());
+
+     Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
+         @Override
+         public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+             return (o1.getValue()).compareTo(o2.getValue());
+         }
+     });
+        HashMap<Character, Integer> temp1 = new LinkedHashMap<>();
+        for (Map.Entry<Character, Integer> aa : list) {
+            temp1.put(aa.getKey(), aa.getValue());
+        }
+        System.out.println(temp1.toString());
     }
-
 }
-
