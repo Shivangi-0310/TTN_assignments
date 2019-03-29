a0d875108d793fbde3f71eeca7d9bf0b7f62ea93
shivangi <shivangi.jain@tothenew.com>
2019-03-29 14:21:33 +0530

    Question 1. Create a class Author with instance variables firstName, lastName and age.

---
+++ hibernateproject-spring-assign/src/main/java/com/hibernate/entity/Author.java
@@ -0,0 +1,58 @@
+package com.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Author {
+    @Id
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
